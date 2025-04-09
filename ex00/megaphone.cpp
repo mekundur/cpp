9987ae3c4aa2ebb6fa60256cada9c85d89e033e0
mekundur <mekundur@c3a1c1.42berlin.de>
2025-04-09 19:20:22 +0200

    some changes

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,21 +6,26 @@
 /*   By: mekundur <mekundur@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 17:11:19 by mekundur          #+#    #+#             */
-/*   Updated: 2025/04/01 17:40:52 by mekundur         ###   ########.fr       */
+/*   Updated: 2025/04/09 18:36:30 by mekundur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <algorithm>
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-int main()
+int main(int argc, char **argv)
 {
-    string str = "Hello Mertcan";
-    cout<<str;
-    reverse(str.begin(), str.end());
-	cout<<"\n"<<str<<"\n";
+
+	std::string str = "Hello Mertcan";
+	std::cout<<str << std::endl;
+	for(int i = 0; str[i]; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32; 
+	}
+	std::cout << str << std::endl;
+//	str = toupper(str);
+  //  	std::cout<<"\n"<<str<<"\n";
     return 0;
 }
+
