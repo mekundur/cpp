d17cc25428db776d4e681b527bd58ddd23a42cac
mekundur <you@example.com>
2025-04-14 12:01:35 +0200

    ex01 directory and Makefile

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -15,17 +15,21 @@
 
 int main(int argc, char **argv)
 {
-
-	std::string str = "Hello Mertcan";
-	std::cout<<str << std::endl;
-	for(int i = 0; str[i]; i++)
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-			str[i] -= 32; 
-	}
-	std::cout << str << std::endl;
-//	str = toupper(str);
-  //  	std::cout<<"\n"<<str<<"\n";
-    return 0;
+		for (int j = 1; argv[j]; j++)
+		{
+			for (int i = 0; argv[j][i]; i++)
+			{
+				if (argv[j][i] >= 97 && argv[j][i] <= 122)
+					argv[j][i] = toupper(argv[j][i]); 
+			}
+			std::cout << argv[j];
+		}
+		std::cout << std::endl;
+	}	
+	return 0;
 }
 
