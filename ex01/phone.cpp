4303397a68835d71f16116d3b63e92c68a3931d3
mekundur <you@example.com>
2025-04-24 16:41:47 +0200

    classes separated into individual files

--- ex01/phone.cpp
+++ ex01/phone.cpp
@@ -14,12 +14,26 @@
 
 int	main(void)
 {
-	/*
+
 	PhoneBook myBook;
-	myBook->contacts[0].first_name = "Mertcan";
+/*	myBook->contacts[0].first_name = "Mertcan";
 	myBook->contacts[0].last_name = "Kunduraci";
 	myBook->contacts[0].phone_number = 4915258437614;
 */
-	Contact random;
-	random.ft_display();
+//	Contact random;
+//	random.ft_display();
+	std::cout << "Welcome to crappy phonebook!" << std::endl;
+	std::string	input;
+	while (1)
+	{
+		std::cout << "Please enter a command ADD or SEARCH or EXIT" << std::endl;
+		std::getline(std::cin, input);
+		if (!input.compare("ADD"))
+			myBook.ADD();
+		else if (!input.compare("SEARCH"))
+			myBook.SEARCH();
+		else if (!input.compare("EXIT"))
+			break;
+	}
+	return (0);
 }
