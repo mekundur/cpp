53c9bab08eda3be748dd00cb876c0ad608ca62b0
mekundur <mertkunn@gmail.com>
2025-09-10 20:56:15 +0200

    cpp07 ex00 and 01 are completed and added

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,23 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <string>
+# include <iostream>
+
+template <typename T>
+void    mutate(T  &elem) {
+    elem++;
+}
+
+template <typename T>
+void    print(const T  &elem) {
+    std::cout << elem << " ";
+}
+
+template <typename T, typename L, typename F> 
+void    iter(T *arr, L len, F func) {
+    for (L i = 0; i < len; i++)
+        func(arr[i]);
+}
+
+#endif
